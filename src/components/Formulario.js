32bbcd8e5bd61c9f69243a7d9e70e3db9e76cee6
David Amador <54488581+dsamador@users.noreply.github.com>
2021-12-07 13:12:51 -0500

    estado e inputs: relacionados

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,25 @@
-
-
+import {useState} from 'react';
 export default function Formulario() {
+
+    const [cita, actualizarCita] = useState({
+            mascota:'',
+            propietario:'',
+            fecha:'',
+            hora:'',
+            sintomas:''
+        })
+
+    //Funcion que se ejecuta cada que el usuario escribe en el input
+    const actualizarState = (e)=>{
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Extraer los valores
+    const {mascota, propietario, fecha, hora, sintomas} = cita;
+
     return (
         <>
             <h2>Crear cita</h2>
@@ -11,6 +30,8 @@ export default function Formulario() {
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre mascota"
+                    onChange={actualizarState}
+                    value={mascota}
                 />
                 
                 <label htmlFor="">Nombre del propietario</label>
@@ -19,29 +40,40 @@ export default function Formulario() {
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del propietario de la mascota"
+                    onChange={actualizarState}
+                    value={propietario}
                 />
 
                 <label htmlFor="">Fecha</label>
                 <input 
                     type="date" 
                     name="fecha"
-                    className="u-full-width"                    
+                    className="u-full-width"
+                    onChange={actualizarState}  
+                    value={fecha}                
                 />
 
                 <label htmlFor="">Hora</label>
                 <input 
                     type="time" 
                     name="hora"
-                    className="u-full-width"                    
+                    className="u-full-width"
+                    onChange={actualizarState}                  
+                    value={hora}
                 />
 
                 <label htmlFor="">Sintomas</label>
                 <textarea
                     className="u-full-width"
-                >
-
-                </textarea>
-
+                    name="sintomas"
+                    onChange={actualizarState}
+                    value={sintomas}
+                ></textarea>
+                
+                <button 
+                    type="submit"
+                    className="u-full-width button-primary"
+                ></button>
             </form>
         </>
     )
